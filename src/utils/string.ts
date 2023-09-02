193ff83fec27ac71284559ce797ca4f2502e9346
danielhssouza <danielsoter3@gmail.com>
2023-09-02 02:48:43 -0300

    feature: adiciona utils

---
+++ src/utils/string.ts
@@ -0,0 +1,15 @@
+function toPascalCase(str: string) {
+    return str
+      .split(' ')
+      .map(word => word.charAt(0).toUpperCase() + word.substring(1).toLowerCase())
+      .join('');
+  }
+  
+  function removeAcentos(str: string) {
+    return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  }
+  
+  export function toPascalCaseSemAcentos(str: string) {
+    const semAcentos = removeAcentos(str);
+    return toPascalCase(semAcentos);
+  }
