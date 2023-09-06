4e29d2f74420e5d8a1ca325ede395f1c54107a64
FernandoAlmeidaPinto <fernando.almeida.pinto@gmail.com>
2023-09-06 15:22:04 -0300

    fix: corrige importacao modulo questao

--- src/modules/questao/questao.module.ts
+++ src/modules/questao/questao.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { QuestaoService } from './questao.service';
 import { QuestaoController } from './questao.controller';
+import { QuestaoRepository } from './questao.repository';
+import { QuestaoSchema } from './questao.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Questao', schema: QuestaoSchema }]),
+  ],
   providers: [QuestaoService],
-  controllers: [QuestaoController]
+  controllers: [QuestaoController, QuestaoRepository],
 })
 export class QuestaoModule {}
