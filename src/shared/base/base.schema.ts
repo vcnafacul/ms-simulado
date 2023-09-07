43e3ae117f10a4b969d580dcb8d780842f1a8d49
FernandoAlmeidaPinto <fernando.almeida.pinto@gmail.com>
2023-09-06 21:54:24 -0300

    fix: remove @Prop de _id

--- src/shared/base/base.schema.ts
+++ src/shared/base/base.schema.ts
@@ -2,7 +2,6 @@ import { Prop, Schema } from '@nestjs/mongoose';
 
 @Schema({ timestamps: true, versionKey: false })
 export class BaseSchema {
-  @Prop({ required: false })
   public _id?: string;
 
   @Prop({ required: false, default: false })
