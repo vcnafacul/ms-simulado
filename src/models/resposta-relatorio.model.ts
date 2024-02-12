6a8a9621cd8f1aa73579cb0ac3d59b63ed703ee0
Laura Fumagalli <laura.rodrigues@radixeng.com.br>
2024-02-12 18:47:57 -0300

    ♻️refactor: remove unuseless files

--- src/models/resposta-relatorio.model.ts
+++
@@ -1,9 +0,0 @@
-import { Questao } from 'src/modules/questao/questao.schema';
-
-export class RespostaRelatorio {
-  constructor(
-    public questao: Questao,
-    public respostaEstudante: string,
-    public alternativaCorreta: string,
-  ) {}
-}
