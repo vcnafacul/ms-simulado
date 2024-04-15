304757678b4146393289f5cf6703afe71d29fafa
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2024-04-15 19:00:54 -0300

    refactor: ajuste para que frente seja identificada por materia

--- src/modules/frente/frente.schema.ts
+++ src/modules/frente/frente.schema.ts
@@ -1,12 +1,18 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { ApiProperty } from '@nestjs/swagger';
 import { BaseSchema } from 'src/shared/base/base.schema';
+import { Materia } from '../materia/materia.schema';
+import { Types } from 'mongoose';
 
 @Schema({ timestamps: false, versionKey: false })
 export class Frente extends BaseSchema {
   @Prop()
   @ApiProperty()
   public nome: string;
+
+  @Prop({ ref: Materia.name, type: Types.ObjectId })
+  @ApiProperty()
+  public materia: Materia;
 }
 
 export const FrenteSchema = SchemaFactory.createForClass(Frente);
